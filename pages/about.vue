24d5c7b6e02a8ba64885e0b80fd9ef31ffbaca3b
unknown <d.redman.cal@gmail.com>
2024-02-12 23:25:54 +0000

    Initial commit

---
+++ pages/about.vue
@@ -0,0 +1,13 @@
+<template lang="">
+    <div>
+        <p>This is the about page</p>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,14 @@
+<template lang="">
+    <div>
+        <p>Hello world this is the home page</p>
+        <button href="about.vue">about</button>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
